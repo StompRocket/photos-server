44c4d9358988898c7603eb0947b79d9cdef22e67
swissChili <sasha@fastmail.com>
2018-05-22 11:21:42 -0700

    Added posting

--- photos.py
+++ photos.py
@@ -11,7 +11,6 @@ images = db.images
 
 image_data = {
 	'ownerid': 'asdf',
-	'ownername': 'dave',
 	'imgid': 'abcd1234',
 	'imgcontent': 'image as bin',
 	'imgtype': 'png'
@@ -24,6 +23,19 @@ print('One image: {0}'.format(result.inserted_id))
 def returnImage(uid, img):
 	image_result = images[uid].find_one({'ownerid': uid, 'imgid': img})
 	return str(image_result)
+@post("/post/<uid>/<img>")
+def postImage(uid, img):
+	images
+	post_content = request.body.read()
+	post_data = {
+		'ownerid': uid,
+		'imgid': img,
+		'imgcontent': post_content.imgcontent,
+		'imgtype': post_content.imgtype
+	}
+	
+	post_result = images[post_data.ownerid].insert_one(post_data)
+	return post_result.inserted_id
 
 
 run(host='localhost', port=4242, debug=True)
